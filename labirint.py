3c18a070b9930af5f6f4dbe590d90c7085f38714
Abobin228 <120650926+Abobin228@users.noreply.github.com>
2022-12-19 15:02:14 +0300

    Update labirint.py

--- labirint.py
+++ labirint.py
@@ -1,25 +1,31 @@
 import pygame
-
+import random
 
 
 pygame.init()
 screen = pygame.display.set_mode((300, 300))
-while True:
-    screen.fill('grey')
-    pygame.draw.line(screen, 'white', (100, 0 ), (100, 300), 5)
-    pygame.draw.line(screen, 'white', (200, 0 ), (200, 300), 5)
-    pygame.draw.line(screen, 'white', (0, 100 ), (300, 100), 5)
-    pygame.draw.line(screen, 'white', (0, 200 ), (300, 200), 5)
-    pygame.draw.line(screen, 'red', (0, 100 ), (100, 100), 5)
-    pygame.draw.line(screen, 'red', (100, 200 ), (200, 200), 5)
-    pygame.draw.line(screen, 'red', (0, 100 ), (100, 100), 5)
-    pygame.draw.line(screen, 'red', (0, 0 ), (0, 300), 5)
-    pygame.draw.line(screen, 'red', (0, 0 ), (300, 0), 5)
-    pygame.draw.line(screen, 'red', (300, 0 ), (300, 300), 5)
-    pygame.draw.line(screen, 'red', (0, 300 ), (300, 300), 5)
+
+screen.fill('grey')
+pygame.draw.line(screen, 'white', (100, 0 ), (100, 300), 5)
+pygame.draw.line(screen, 'white', (200, 0 ), (200, 300), 5)
+pygame.draw.line(screen, 'white', (0, 100 ), (300, 100), 5)
+pygame.draw.line(screen, 'white', (0, 200 ), (300, 200), 5)
+pygame.draw.line(screen, 'red', (0, 100 ), (100, 100), 5)
+pygame.draw.line(screen, 'red', (100, 200 ), (200, 200), 5)
+pygame.draw.line(screen, 'red', (0, 100 ), (100, 100), 5)
+pygame.draw.line(screen, 'red', (0, 0 ), (0, 300), 5)
+pygame.draw.line(screen, 'red', (0, 0 ), (300, 0), 5)
+pygame.draw.line(screen, 'red', (300, 0 ), (300, 300), 5)
+pygame.draw.line(screen, 'red', (0, 300 ), (300, 300), 5)
+running = True
+while running:
+    # Держим цикл на правильной скорости
+# Ввод процесса (события)
+    for event in pygame.event.get():
+# check for closing window
+        if event.type == pygame.QUIT:
+            running = False
     pygame.display.flip()
-
-
 
 
 
